e0b72339da54edb8ca54f928f121e732c259938f
Gage Malmgren <88338320+TheRageWithGage@users.noreply.github.com>
2023-05-14 16:18:17 -0600

    no javascript
    turns out I don't need javascript to get what I want, thanks css

--- essentials/script.js
+++ essentials/script.js
@@ -1,12 +1,16 @@
-      window.onload = function() {
-        var overlay = document.getElementById('overlay');
-        var opacity = 1;
-        var interval = setInterval(function() {
-            console.log('overlay loaded');
-            opacity -= 0.01;
-            // overlay.style.backgroundColor = 'rgba(0, 0, 0, ' + opacity + ')'; // Set the background color to a darker opacity
-          if (opacity <= 0.5) {
-            clearInterval(interval); // Stop the interval once the desired opacity is reached
-          }
-        }, 20); // Change the opacity every 20 milliseconds
-      }
+      // window.onload = function() {
+      //   var overlay = document.getElementById('overlay');
+      //   var opacity = 1;
+      //   var myText = document.getElementById('fade-text');
+      //   myText.style.opacity = '1';
+      //   var interval = setInterval(function() {
+      //       console.log('overlay loaded');
+      //       opacity -= 0.01;
+      //       // overlay.style.backgroundColor = 'rgba(0, 0, 0, ' + opacity + ')'; // Set the background color to a darker opacity
+      //     if (opacity <= 0.5) {
+      //       clearInterval(interval); // Stop the interval once the desired opacity is reached
+      //     }
+      //   }, 20); 
+      // }
+
+  
